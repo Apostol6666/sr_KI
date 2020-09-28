69dd52171a34a3b793187dc3c959168889b9efb3
Apostol6666 <44125687+Apostol6666@users.noreply.github.com>
2020-09-28 16:41:39 +0800

    Create a.asm

---
+++ a.asm
@@ -0,0 +1,153 @@
+%macro pushd 0
+    push edx
+    push ecx
+    push ebx
+    push eax
+%endmacro
+
+%macro popd 0
+    pop eax
+    pop ebx
+    pop ecx
+    pop edx
+%endmacro
+
+%macro print 2
+    pushd
+    mov edx, %1
+    mov ecx, %2
+    mov ebx, 1
+    mov eax, 4
+    int 0x80
+
+    popd
+%endmacro
+
+%macro dprint 0
+    pushd
+    mov ecx, 10
+    mov bx, 0
+    
+
+    %%_divide:
+        mov edx,0
+        div ecx
+        push dx
+        inc bx
+        test eax, eax
+        jnz %%_divide
+        
+    mov cx,bx
+    
+    %%_digit:
+        pop ax
+        add ax, '0'
+        mov [count], ax
+        print 1, count
+        dec cx
+        mov ax, cx
+        cmp cx, 0
+        jg %%_digit
+    
+    popd
+%endmacro     
+
+
+section .text
+
+global _start
+
+_start:
+    
+    ;поиск суммы 1 массива
+    mov bx, 0
+    mov eax, 0 
+    
+    
+_loop:
+    add eax, [x + ebx]
+    
+    add bx, 4
+cmp bx, alen
+jne _loop
+     
+    mov [count2], eax
+    
+    mov eax, [count2]
+  
+    
+    
+    ; поиск суммы 2 массива
+    mov bx, 0
+    mov eax, 0  
+    
+_zaloop:
+    add eax, [y + ebx]
+    
+    add bx, 4
+cmp ebx, alen2
+jne _zaloop
+
+    mov [count3], eax
+    
+    mov eax, [count3]
+  
+    
+    ; деление по размерности
+    mov edx, 0
+    mov eax, alen
+    mov ecx, 4
+    div ecx
+    
+    mov [c], eax
+
+    ;среднее арифметическое первого массива
+    mov edx, 0
+    mov eax, [count2]
+    mov ecx, [c]
+    div ecx
+    
+    mov [a], eax
+   
+   ;среднее арифметическое второго массива 
+    mov edx, 0
+    mov eax, [count3]
+    mov ecx, [c]
+    div ecx
+    
+    mov [b], eax
+    
+    ;разность
+    mov ax, [b]
+    mov bx, [a]
+    sub ax, bx
+    dprint
+    
+    print nlen, newline
+    print len, message
+    print nlen, newline
+
+    mov eax, 1
+    int 0x80
+
+section .data
+    x dd 5, 3, 2, 6, 1, 7, 4
+    alen equ $ - x
+    
+    y dd 0, 10, 1, 9, 2, 8, 5
+    alen2 equ $ - y
+    
+
+    message db "Done"
+    len equ $ - message
+    
+    newline db 0xA, 0xD
+    nlen equ $ - newline
+    
+section .bss
+    count resd 1
+    count2 resd 1 ;сумма первого массива
+    count3 resd 1 ;сумма второго массива
+    c resd 1 ;длина массива
+    a resd 1 ;первое среднее ариф
+    b resd 1 ;второе среднее ариф
